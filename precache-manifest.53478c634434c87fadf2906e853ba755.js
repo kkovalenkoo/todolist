1c9c450872d12a167215493f9b08177f2306c1a9
kkovalenkoo <yvkovalenko1987@gmail.com>
2021-09-26 20:13:49 +0300

    Updates

---
+++ precache-manifest.53478c634434c87fadf2906e853ba755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7fe29bc1cec663d0d47d861cc40d3b",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a7ab432f34a98cbf81b9",
+    "url": "/todolist/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "ba6f9d2fcfda00a49e16",
+    "url": "/todolist/static/js/2.bc596599.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.bc596599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ab432f34a98cbf81b9",
+    "url": "/todolist/static/js/main.38f5bbb9.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
