8ebbd082e5de26c140a91ffe4148a22d7d4088b7
kkovalenkoo <yvkovalenko1987@gmail.com>
2021-10-07 22:27:33 +0300

    reducers + tests + tdd

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent} from 'react'
-import {FilterType, TaskType} from './App'
+import {FilterType, TaskType} from '../App'
 import {AddItemForm} from './AddItemForm'
 import {EditableSpan} from './EditableSpan'
 import {Button, Checkbox, IconButton} from '@material-ui/core'
